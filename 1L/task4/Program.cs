f121ef5130846c6f4eb956ef8bb3d4b622af496e
Anastasia Mazina <MazinaAnastasiaC@gmail.com>
2024-03-19 21:01:48 +0000

    git init

---
+++ 1L/task4/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+
+int N = new Random().Next(0,100000);
+Console.WriteLine(N);
+
+
+if(N < 10)
+{
+    Console.WriteLine(N);
+}
+else
+{
+    while(N > 0) //256
+    {
+        int R = N % 10;
+        N /= 10;
+        if (N > 0)
+        {
+            Console.Write($"{R}, ");
+        }
+        else
+        {
+            Console.Write($"{R}");
+        }
+    }
+}
+
+// else
+// {
+//     // Создаем массив для хранения цифр числа
+//     int[] digits = new int[N.ToString().Length];
+//     int index = 0;
+
+//     while (N > 0)
+//     {
+//         int R = N % 10;
+//         N /= 10;
+//         digits[index] = R; // Сохраняем цифру в массиве
+//         index++;
+//     }
+
+//     // Выводим цифры в обратном порядке
+//     for (int i = digits.Length - 1; i >= 0; i--)
+//     {
+//         Console.Write($"{digits[i]}");
+//         if (i > 0)
+//         {
+//             Console.Write(", ");
+//         }
+//     }
+// }
+
