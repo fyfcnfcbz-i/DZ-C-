04a74bc0a5f546f62d88d41a316428b2c99e879d
Anastasia Mazina <MazinaAnastasiaC@gmail.com>
2024-03-22 20:22:32 +0000

    add info task4

--- 1L/task4/Program.cs
+++ 1L/task4/Program.cs
@@ -1,30 +1,30 @@
 ﻿// Напишите программу, которая на вход принимает натуральное число N, 
 // а на выходе показывает его цифры через запятую.
 
-int N = new Random().Next(0,100000);
-Console.WriteLine(N);
+// int N = new Random().Next(0,100000);
+// Console.WriteLine(N);
 
 
-if(N < 10)
-{
-    Console.WriteLine(N);
-}
-else
-{
-    while(N > 0) 
-    {
-        int R = N % 10;
-        N /= 10;
-        if (N > 0)
-        {
-            Console.Write($"{R}, ");
-        }
-        else
-        {
-            Console.Write($"{R}");
-        }
-    }
-}
+// if(N < 10)
+// {
+//     Console.WriteLine(N);
+// }
+// else
+// {
+//     while(N > 0) 
+//     {
+//         int R = N % 10;
+//         N /= 10;
+//         if (N > 0)
+//         {
+//             Console.Write($"{R}, ");
+//         }
+//         else
+//         {
+//             Console.Write($"{R}");
+//         }
+//     }
+// }
 
 // else
 // {
@@ -50,4 +50,24 @@ else
 //         }
 //     }
 // }
+
+
+Console.Write("Введите трехзначное целое число N: ");
+string N = Console.ReadLine()!; // переменная N принимает значение string, 
+                                //т.к конструкция Console.ReadLine() имеет строковое значение 
+                                //и отсутсвует Конвектор Convert.ToInt32
+int i = 0;
+
+while (i < N.Length)
+{
+    if (i < N.Length - 1)
+    {
+        Console.Write($"{N[i]}, ");
+    }
+    else
+    {
+        Console.Write($"{N[i]}");
+    }
+    i++;
+}
 
